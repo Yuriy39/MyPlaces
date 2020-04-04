9c86052048603ee33a85b14416e3a516d203803b
Yuriy39 <fedechko.yursi@gmail.com>
2020-04-04 17:03:23 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Юрій Федечко on 04.04.2020.
+//  Copyright © 2020 Юрій Федечко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
